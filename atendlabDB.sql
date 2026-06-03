9d69308a8ccbcab3fd242e8c08b3e792526befc3
Ellen <107266328+Izillen@users.noreply.github.com>
2026-06-03 17:09:34 -0300

    Add files via upload

---
+++ atendlabDB.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS atendelab
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_general_ci;
+USE atendelab;
+
+CREATE TABLE usuarios (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nome VARCHAR(100) NOT NULL,
+ email VARCHAR(100) NOT NULL UNIQUE,
+ senha VARCHAR(255) NOT NULL,
+ perfil ENUM('admin', 'atendente') DEFAULT 'atendente',
+ status ENUM('ativo', 'inativo') DEFAULT 'ativo',
+ criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
