e69d180752527da22508278444f3de5c63d1b2f7
izillen <beatrizlyrabarreto@gmail.com>
2026-06-03 19:35:38 -0300

    parte1

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AtendeLab</title>
+</head>
+<body>
+    <div class="">
+        <h1>Bem-vindo ao AtendeLab</h1>
+        <p>Este é o ponto de entrada da aplicação.</p>
+    </div>
+</body>
+</html>
